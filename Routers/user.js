fb781ee7ce507ba428545be775208f276242b909
Clayton Parker <claytonericparker@gmail.com>
2023-11-27 14:54:16 -0500

    Help

--- Routers/user.js
+++ Routers/user.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const router = new express.Router()
 const bcrypt = require('bcrypt');
-const saltRounds = 10;
 
 const User = require("../models/user")
 
@@ -35,4 +34,24 @@ else{
     res.send("Password no match")
 }  
 })
+
+router.post('/Login',async (req,res)=>{
+    let username = req.body.registerUsername
+    let password = req.body.registerPassword//gets password
+    //step 1
+    const foundUser = await User.findOne({username: username})//finds the user from username
+    if ((foundUser==null)) {
+    try {
+        res.send("No user found with that username! Or you got your password wrong! Go back and try again!")
+    } catch (e) {
+        console.log(e)
+    }
+
+    console.log(req.body)
+}
+else{
+    
+}
+})
+
 module.exports = router;
