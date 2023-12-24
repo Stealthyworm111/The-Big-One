fbde0216b5f432b50540d4fbc3dbafd4a83f11d6
Clayton Parker <70655862+Stealthyworm111@users.noreply.github.com>
2023-12-23 23:16:05 -0500

    heell2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 require('dotenv').config()
 const path = require('path');
 const mongoose = require('mongoose')
-const userRouter = require("./Routers/user")
+const userRouter = require("./routers/user")
 const app = express()
 const port = 3000
 app.listen(port)
